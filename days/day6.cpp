86cec71864d62f361714aabba1bfca34b554295f
Ana Santos <anaezes@gmail.com>
2021-04-05 10:34:39 +0100

    Add 2nd part of day6

--- days/day6.cpp
+++ days/day6.cpp
@@ -11,22 +11,39 @@ void day6() {
     std::vector<std::set<char>> answers;
     std::string line;
 
-    int count = 0;
+    int count1 = 0;
+    int count2 = 0;
     while(std::getline(file_in, line))
     {
-        std::set<char> answer;
-        for(int i = 0; i < line.size(); i++)
-            answer.insert(line[i]);
+        std::set<char> answer1;
+        std::vector<std::pair<char, int>> answer2;
+        int nGroup = 1;
+
+        for(int i = 0; i < line.size(); i++) {
+            answer1.insert(line[i]);
+            answer2.push_back({line[i], 1});
+        }
         while (std::getline(file_in, line)) {
             if(line.empty() || line[0] == '\n'){
                 break;
             }
-            for(int i = 0; i < line.size(); i++)
-                answer.insert(line[i]);
-
+            nGroup++;
+            for(int i = 0; i < line.size(); i++) {
+                answer1.insert(line[i]);
+                for(int j = 0; j < answer2.size(); j++) {
+                    if(answer2.at(j).first == line[i])
+                        answer2.at(j).second++;
+                }
+            }
         }
-        count += answer.size();
+        count1 += answer1.size();
+        for(auto a: answer2)
+        {
+            if(a.second == nGroup)
+                count2 ++;
+        }
     }
 
-    std::cout << "Part One: " << count << std::endl;
+    std::cout << "Part One: " << count1 << std::endl;
+    std::cout << "Part Two: " << count2 << std::endl;
 }
